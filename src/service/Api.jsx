ce83947e5f8387fb6114c535a5fa5b9ce7d57ad0
Debraj Dhar <debrajdhar100@gmail.com>
2023-08-14 06:18:17 +0530

    fixes letter casing

---
+++ src/service/Api.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+const baseUrl = "https://apimocha.com/quicksell/data";
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get(baseUrl);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export default fetchData;
